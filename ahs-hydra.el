f545db50219437827e08866ad6f490efea6ddd0e
Brett Wines <bwines@quip.com>
2020-06-23 21:58:27 -0700

    working hydra

--- ahs-hydra.el
+++ ahs-hydra.el
@@ -1,14 +1,69 @@
 ;;; ~/doom-config/ahs-hydra.el -*- lexical-binding: t; -*-
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 (defhydra hydra-auto-symbol-highlight (global-map "<f2>")
-  ("d" ahs-forward-definition "next definition")
-  ("D" ahs-backward-definition "previous definition")
-  ("e" ahs-to-iedit "iedit")
-  ("n" ahs-forward "next")
+  ("n" quick-ahs-forward "next")
   ("N" ahs-backward "previous")
-  ("p" ahs-backward "previous")
-  ("R" ahs-back-to-start "reset")
-  ("r" ahs-change-range "change range")
+  ("p" quick-ahs-backward "previous")
+;;  ("d" ahs-forward-definition "next definition")
+;;  ("D" ahs-backward-definition "previous definition")
+;;  ("r" ahs-change-range "change range")
+;;  ("R" ahs-back-to-start "reset")
+  ("z" (progn (recenter-top-bottom) (ahs)) "recenter")
+;;  ("e" ahs-to-iedit "iedit")
+;;  ("s" swoop "swoop")
+;;  ("b" buffers "buffers")
+;;  ("b" buffers "buffers")
+;;  ("f" files "files")
+;;  ("/" project "project")
   ("q" nil :exit t))
 
 (defun ahs ()
@@ -19,6 +74,7 @@
     )
   (ahs-highlight-now)
   (hydra-auto-symbol-highlight/body))
+(global-set-key (kbd "C-c a h s") 'ahs)
 
 (defun quick-ahs-forward ()
   "Go to the next occurrence of symbol under point with `auto-highlight-symbol'"
@@ -30,12 +86,42 @@
   (interactive)
   (quick-ahs-move nil))
 
+;; problem: only cycles around within visible segment of buffer
 (defun quick-ahs-move (forward)
   "Go to the next occurrence of symbol under point with `auto-highlight-symbol'"
   (if forward
       (progn
+        (ahs-highlight-now)
         (hydra-auto-symbol-highlight/body)
         (ahs-forward))
     (progn
+      (ahs-highlight-now)
       (hydra-auto-symbol-highlight/body)
       (ahs-backward))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(defun aoeu ()
+  (message "'sup"))
