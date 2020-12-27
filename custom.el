bf9d1a68b53339957039e2778cacb4a3398e6281
Brett Wines <bwines@quip.com>
2020-12-27 17:28:33 -0500

    commit `custom.el`

---
+++ custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(helm-swoop symbol-navigation-hydra))
+ '(recentf-auto-cleanup 60)
+ '(safe-local-variable-values '((git-commit-major-mode . git-commit-elisp-text-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t :height 3.5 :foreground "deep sky blue" :inherit 'aw-leading-char-face)))
+ '(company-preview-common ((t (:background "#21e824bc35b0"))))
+ '(company-scrollbar-bg ((t (:background "#2bd12f784561"))))
+ '(company-scrollbar-fg ((t (:background "#21e824bc35b0"))))
+ '(company-tooltip ((t (:inherit default :background "#1bf61e4b2c46"))))
+ '(company-tooltip-annotation ((t (:foreground "deep sky blue"))))
+ '(company-tooltip-annotation-selection ((t (:inherit company-tooltip-annotation :foreground "deep sky blue" :weight bold))))
+ '(company-tooltip-common ((t (:inherit font-lock-constant-face))))
+ '(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
+ '(helm-selection ((t :background "gray25" :distant-foreground "black" :foreground "white smoke"))))
