a007700fbff33fed63b350c3a84f790f87839dc8
Brett Wines <bwines@bwines-ltmcx3z.internal.salesforce.com>
2024-09-09 11:05:24 -0700

    more yasnippet

--- yasnippets/emacs-lisp-mode/.yas-compiled-snippets.el
+++ yasnippets/emacs-lisp-mode/.yas-compiled-snippets.el
@@ -2,7 +2,7 @@
 ;;; Snippet definitions:
 ;;;
 (yas-define-snippets 'emacs-lisp-mode
-                     '(("msg" "(message (format \"$1: %s\" $1))" "msg" nil nil nil "/Users/bwines/.config/doom/yasnippets/emacs-lisp-mode/msg" nil nil)))
+                     '(("msg" "(message (format \"$1: %s\" $1))" "msg" nil nil nil "/Users/bwines/doom-config/yasnippets/emacs-lisp-mode/msg" nil nil)))
 
 
-;;; Do not edit! File generated at Tue Mar  5 22:35:50 2024
+;;; Do not edit! File generated at Wed Feb 28 16:23:14 2024
--- yasnippets/snippet-mode/.yas-compiled-snippets.el
+++ yasnippets/snippet-mode/.yas-compiled-snippets.el
@@ -2,7 +2,7 @@
 ;;; Snippet definitions:
 ;;;
 (yas-define-snippets 'snippet-mode
-                     '(("cccd" "async with core.coro.concurrent_dict() as batch:\n    $1" "cccd" nil nil nil "/Users/bwines/.config/doom/yasnippets/snippet-mode/cccd" nil nil)))
+                     '(("cccd" "async with core.coro.concurrent_dict() as batch:\n    $1" "cccd" nil nil nil "/Users/bwines/doom-config/yasnippets/snippet-mode/cccd" nil nil)))
 
 
-;;; Do not edit! File generated at Tue Mar  5 22:35:50 2024
+;;; Do not edit! File generated at Wed Feb 28 16:23:14 2024
